55f6674402c196e23172cc6d9f3820c581532879
Gowthamswamypalika <120080083+Gowthamswamypalika@users.noreply.github.com>
2023-08-12 10:46:20 +0530

    import java.util.Scanner; public class cir{     public static void main(String[] ar)     {         Scanner c=new Scanner(System.in);//Scanner Objectname         double r=c.nextDouble();//datatype var name=objectname         double a=(3.14)*r*r;         System.out.printf("%.2f",a);//(var name)
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class cir{
+    public static void main(String[] ar)
+    {
+        Scanner c=new Scanner(System.in);//Scanner Objectname
+        double r=c.nextDouble();//datatype var name=objectname
+        double a=(3.14)*r*r;
+        System.out.printf("%.2f",a);//(var name)
+        
+    }
+}
