b967c8c1ab498d9cdc39a0f635acaad4fdd9def0
Gowthamswamypalika <120080083+Gowthamswamypalika@users.noreply.github.com>
2023-08-17 11:37:17 +0530

    import java.util.Scanner; public class main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char a=sc.next().charAt(0);         if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')         {             System.out.print("VOWEL");         }         else         {             System.out.print("CONSONANT");         }     }
    }

---
+++ VOWEL_or_CONSONANT.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+        {
+            System.out.print("VOWEL");
+        }
+        else
+        {
+            System.out.print("CONSONANT");
+        }
+    }
+
+    
+}
