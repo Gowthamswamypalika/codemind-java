12a6ae00def6f0e1ac0bbdcc3924cc96a3ea96f7
Gowthamswamypalika <120080083+Gowthamswamypalika@users.noreply.github.com>
2023-08-18 11:28:50 +0530

    import java.util.Scanner; public class main{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i;         int a=sc.nextInt();         for(i=1;i<=a;i++)         {             System.out.print(i + " ");         }     } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i;
+        int a=sc.nextInt();
+        for(i=1;i<=a;i++)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
